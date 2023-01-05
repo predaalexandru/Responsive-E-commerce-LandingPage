844e372443bc721353d3f51427ebfe03fc3acbf0
Preda Alexandru <alexpreda95@yahoo.com>
2023-01-05 15:53:10 +0200

    Project setup

---
+++ e-commerce/src/App.js
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <div className="App">
+     <Nav />
+     <Hero />
+     <Products />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
